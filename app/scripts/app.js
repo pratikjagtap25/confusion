cb9612ec399d088585685168a8e9690a89fb2be2
System Administrator <root@Ashishs-Mac-mini.local>
2017-03-15 11:30:24 +0530

    Changed To Gulp implementation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('confusionApp',[])
-	.controller('menuController',function(){
+	.controller('MenuController',['$scope',function($scope){
 
-      this.tab=1;
+      $scope.tab=1;
 
-      this.filterText="";
+      $scope.filterText="";
 
-      var dishes=[
+      $scope.showDetails=false;
+
+      $scope.dishes=[
 	             {
 	               name:'Uthapizza',
 	               image: 'images/uthapizza.png',
@@ -45,26 +47,27 @@ angular.module('confusionApp',[])
 	               comment: ''
 	            }
 	      ];
-		this.dishes=dishes;
-
-		this.select=function(setTab){
-		  this.tab=setTab;
+		$scope.select=function(setTab){
+		  $scope.tab=setTab;
 
 		  if (setTab === 2) {
-		      this.filtText = "appetizer";
+		      $scope.filtText = "appetizer";
 		    } 
 		    else if (setTab === 3) {
-		      this.filtText = "mains";
+		      $scope.filtText = "mains";
 		    }
 		    else if (setTab === 4) {
-		      this.filtText = "dessert";
+		      $scope.filtText = "dessert";
 		    }
 		    else {
-		      this.filtText = "";
+		      $scope.filtText = "";
 		    }
 		};
-
-		this.isSelected=function(checkTab){
-		  return (this.tab === checkTab);
+		$scope.toggleDetails=function()
+		{
+			$scope.showDetails= !$scope.showDetails;
+		}
+		$scope.isSelected=function(checkTab){
+		  return ($scope.tab === checkTab);
 		};
-});
+}]);
